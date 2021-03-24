f36a6b25800f1bdaca8b65ad512061f6019bf19d
GYEISSON <yeison7f@gmail.com>
2021-03-24 14:40:48 -0500

    aplicacion funcionando

--- src/main/java/edu/escuelaing/arem/arep/App.java
+++ src/main/java/edu/escuelaing/arem/arep/App.java
@@ -1,13 +1,53 @@
 package edu.escuelaing.arem.arep;
+import static spark.Spark.*;
 
+import java.text.ParseException;
+import java.util.Scanner;
+
+import org.eclipse.jetty.util.log.Log;
+import org.json.simple.JSONObject;
+
+import spark.Request;
+import spark.Response;
+
+import  edu.escuelaing.arem.arep.models.Calculator;
 /**
- * Hello world!
+ * Hello Spark!
  *
+ *  web app  Heroku using SparkWeb
+ *
+ * @author daniel
+ * modified by @author Yeisson Gualdron
  */
-public class App 
+public class App
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+  private static Calculator cal;
+
+  public static void main( String[] args )
+  {
+
+      System.out.println( "Hello Math!" );
+      port(getPort());
+      cal = new Calculator();
+      get("/atan", (req, res) -> inputDataPage(req, res,"atan"));
+      get("/exp", (req, res) -> inputDataPage(req, res,"exp"));
+
+
+  }
+  private static JSONObject inputDataPage(Request req, Response res, String operation)  {
+      double number = Double.parseDouble(req.queryParams("value"));
+      JSONObject obj=new JSONObject();    
+      obj.put("operation",operation);    
+      obj.put("input", number);    
+      obj.put("output",(operation.equals("atan"))? cal.arcTangent(number) : cal.eulerTo(number));    
+      return obj;
+  }
+
+  public static  int getPort(){
+      if (System.getenv("PORT") != null) {
+          return Integer.parseInt(System.getenv("PORT"));
+      }
+
+      return 4567;
+  }
 }
---
+++ src/main/java/edu/escuelaing/arem/arep/models/Calculator.java
@@ -0,0 +1,14 @@
+package edu.escuelaing.arem.arep.models;
+
+import java.lang.Math.*;
+
+
+public class Calculator{
+
+  public double arcTangent(double number){
+    return Math.atan(number);
+  }
+  public double eulerTo(double number){
+    return Math.exp(number);
+  }
+}
